16f5f5cfa2416fda243143874b382509504f1556
Nader Ebrahim <nebrahim2424@gmail.com>
2024-04-02 14:30:47 -0700

    added .env file, small fix in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         </NextThemesProvider>
         <Script
           strategy="beforeInteractive"
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&callback=Function.prototype`}
+          src={`http://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&callback=Function.prototype`}
         />
         <Analytics />
       </body>
