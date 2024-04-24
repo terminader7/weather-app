803aafe07eaaf80686068408b16af58847f9c6dc
Nader Ebrahim <nebrahim2424@gmail.com>
2024-04-24 16:20:47 -0700

    testing possible deployment fixes

--- actions/getFiveDayForecast.ts
+++ actions/getFiveDayForecast.ts
@@ -8,7 +8,7 @@ export const getFiveDayForecast = async ({
   const protocol =
     process.env.NODE_ENV !== "development" ? "https://" : "http://";
   const data = await fetch(
-    `${protocol}${process.env.VERCEL_URL}/api/hourly?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&units=imperial`
+    `${protocol}/api/hourly?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&units=imperial`
   );
   if (!data.ok) {
     throw new Error("Failed to fetch daily forecast data");
